1a7bbf8bc8e173fad6b51d87c16dcd6b9e664eb4
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-08-08 18:38:57 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Zadanie 1
+print('Zadanie 1')
+print('')
+lista_zakupów = {
+    'Piekarnia' : ['Chleb', 'Bułki', 'Pączek'],
+    'Warzywniak' : ['Marchew', 'Seler', 'Rukola']
+}
+print('Lista Zakupów')
+for klucz, wartosc in lista_zakupów.items():
+    print(f"Idę do {klucz} i kupuję tam następujące rzeczy: {wartosc}")
+print(f"W sumie kupiłem {len(lista_zakupów['Piekarnia']) + len(lista_zakupów['Warzywniak'])} produktów.")
+print('')
